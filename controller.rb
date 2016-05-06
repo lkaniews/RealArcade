e378845c65a3f4b07650a7e6f83d2fec9a129aae
Liza Kaniewski <lkaniews@lsoc.org>
2016-05-06 11:59:27 -0500

    May 6 updates

--- controller.rb
+++ controller.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
+require_relative 'models/rps.rb'
 
 get '/' do 
 	erb :index
-end
+end 
 
+post '/' do 
+	p1 = Player.new(params[:name])
+	p2 = Player.new(params[:name2])
+	game = Game.new(p1, p2)
+	p1.choice = params[:choice]
+	p2.choice = params[:choice]
+	game.scenario
+end
--- models/rps.rb
+++ models/rps.rb
@@ -1,1 +1,48 @@
+class Player 
+	attr_accessor :name, :choice
+	def initialize (name)
+		@name = name
+		@wins = 0
+	end
+end
 
+
+class Game 
+		def initialize (player_one, player_two)
+			@player1 = player_one
+			@player2 = player_two
+			
+	end
+
+	def scenario
+		if @player1.choice == "rock" && @player2.choice == "scissors" 
+			"player one wins!" 
+	
+		elsif @player1.choice == "rock" && @player2.choice == "paper"
+			 "player two wins!"
+
+		elsif @player1.choice == "rock" && @player2.choice == "rock"
+			"Tie"
+
+		elsif @player1.choice == "paper" && @player2.choice == "scissors" 
+			 "player two wins!" 
+	
+		elsif @player1.choice == "paper" && @player2.choice == "rock"
+			"player one wins!"
+
+		elsif @player1.choice == "paper" && @player2.choice == "paper"
+			"player one wins!"
+		
+		elsif @player1.choice == "scissors" && @player2.choice == "paper" 
+			"player once wins!" 
+	
+		elsif @player1.choice == "scissors" && player2.choice == "rock"
+			"player two wins!"
+
+		elsif @player1.choice == "scissors" && @player2.choice == "scissors"
+			"Tie!"
+		else
+			"nadda"
+		end
+	end
+end
